1949ff885d7e87d684e2032e848b759630082503
Armin Reichert <armin.reichert@web.de>
2020-05-24 07:37:31 +0200

    simplified

--- BirdyGame/src/main/java/de/amr/games/birdy/entities/Score.java
+++ BirdyGame/src/main/java/de/amr/games/birdy/entities/Score.java
@@ -31,22 +31,13 @@ public class Score extends Entity implements View {
 			BufferedImage digitImage = Assets.image("number_score_0" + d);
 			digits[d] = digitImage.getScaledInstance(-1, round(scale) * digitImage.getHeight(), Image.SCALE_SMOOTH);
 		}
-		updateText();
-	}
-
-	private String pointsText() {
-		return String.format("%d", score.get());
-	}
-
-	private void updateText() {
-		scoreText = pointsText();
-		tf.width = (scoreText.length() * digits[0].getWidth(null));
-		tf.height = (digits[0].getHeight(null));
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
-		updateText();
+		scoreText = String.format("%d", score.get());
+		tf.width = (scoreText.length() * digits[0].getWidth(null));
+		tf.height = (digits[0].getHeight(null));
 		for (int i = 0; i < scoreText.length(); i++) {
 			int digit = "0123456789".indexOf(scoreText.charAt(i));
 			g.drawImage(digits[digit], (int) tf.x + i * (digits[0].getWidth(null) - round(3 * scale)), (int) tf.y, null);
